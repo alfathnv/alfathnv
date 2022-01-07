72f9605fb5ae2085260d5bd4f92226c76bd4059e
Alfath Noverio <alfathnv@gmail.com>
2022-01-07 07:13:44 +0700

    Create Navbar and add font awesome

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,15 @@
-import Box from '../components/Box'
-import React from 'react'
+import Box from '../components/Box';
+import React from 'react';
+import './Home.scss';
+import Content from '../components/Content';
 
 function Home() {
   return (
-    <>
-      <Box />
-    </>
+    <div className="Main">
+      <Content className="content" />
+      <Box className="box" />
+    </div>
   )
 }
 
-export default Home
+export default Home;
